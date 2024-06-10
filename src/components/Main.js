2ef59ff238a258b76ac667fc6df8aaf1b1e237cc
feliciasusanto <feliciasusanto.fs@gmail.com>
2024-06-10 22:37:18 +0700

    popular books cards, split up search form inputs from main and share state between child component Search Form and parent component Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,103 +1,112 @@
 import React from 'react'
+import SearchForm from './SearchForm.js'
 import Cards from './Cards.js'
 
 class Main extends React.Component {
-    constructor(props){
-        super(props)
-        this.booksMaster = [
-            {
-                "title": "Classical Mythology",
-                "author": "Mark P. O. Morford",
-                "publisher": "Oxford University Press",
-                "year": 2022
-            },
-            {
-                "title": "Rules of The Wild",
-                "author": "Francesca Marciano",
-                "publisher": "Random House Inc",
-                "year": 1998
-            },
-            {
-                "title": "Clara Callan",
-                "author": "Richard Bruce Wright",
-                "publisher": "Harper Flamingo Canada",
-                "year": 2001
-            },
-            {
-                "title": "Wild Animus",
-                "author": "Rich Shapero",
-                "publisher": "Too Far",
-                "year": 2004
-            },
-            {
-                "title": "Tage der Unschuld",
-                "author": "Richard North Patterson",
-                "publisher": "Goldmann",
-                "year": 2000
-            },
-            {
-                "title": "The Hellfire Club",
-                "author": "Peter Straub",
-                "publisher": "Random House Inc",
-                "year": 1996
-            },
-            {
-                "title": "The Night Listener",
-                "author": "Armistead Maupin",
-                "publisher": "Harper Colins Publishers",
-                "year": 2000
-            },
-            {
-                "title": "Night Tales",
-                "author": "Nora Roberts",
-                "publisher": "Silhouette",
-                "year": 2000
-            }]
-        }
-        
-        render() {
-            return (
-                <main className="container ">
-                    <div className="form-group">
-                        <div className="row mx-auto my-3">
-                            <input type="text" name="search" id="search" placeholder="Search" className="form-control col"/><br/>
-                        </div>
-                        <div className="row mx-auto my-3">
-                            <div className="col row" style={{lineHeight: '2.5'}}>
-                                <div className="form-check col-1 px-0" style={{display: 'table-cell',verticalAlign: 'middle'}}>
-                                    <input type="radio" name="searchBy" id="title" /> <label htmlFor="title">Title</label>
-                                </div>
-                                <div className="form-check col-1 px-0">
-                                    <input type="radio" name="searchBy" id="author" /> <label htmlFor="author">Author</label>
-                                </div>
-                                <div className="form-check col-1 px-0">
-                                    <input type="radio" name="searchBy" id="publisher"/> <label htmlFor="publisher">Publisher</label>
-                                </div>
-                                <div className="form-check col px-0">
-                                    <div className="col" style={{display: 'inline-block'}}>
-                                        <input type="radio" name="searchBy" id="titleYear"/> <label htmlFor="titleYear">Title and Year = </label>        
-                                    </div>
-                                    <div className="col" style={{display:'inline-block'}}>
-                                        <input type="text" name="year" id="year" className="form-control"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-        
-                        <div className="row">
-                            <h1 className='h3'>Our Books</h1>
-                            <hr></hr>
-                        </div>
+	constructor(props) {
+		super(props)
+		this.state = { search: '', searchBy: '', year: 1900 }
+		this.booksMaster = [
+			{
+				title: 'Classical Mythology',
+				author: 'Mark P. O. Morford',
+				publisher: 'Oxford University Press',
+				year: 2022,
+				popular: false,
+			},
+			{
+				title: 'Rules of The Wild',
+				author: 'Francesca Marciano',
+				publisher: 'Random House Inc',
+				year: 1998,
+				popular: true,
+			},
+			{
+				title: 'Clara Callan',
+				author: 'Richard Bruce Wright',
+				publisher: 'Harper Flamingo Canada',
+				year: 2001,
+				popular: false,
+			},
+			{
+				title: 'Wild Animus',
+				author: 'Rich Shapero',
+				publisher: 'Too Far',
+				year: 2004,
+				popular: true,
+			},
+			{
+				title: 'Tage der Unschuld',
+				author: 'Richard North Patterson',
+				publisher: 'Goldmann',
+				year: 2000,
+				popular: false,
+			},
+			{
+				title: 'The Hellfire Club',
+				author: 'Peter Straub',
+				publisher: 'Random House Inc',
+				year: 1996,
+				popular: false,
+			},
+			{
+				title: 'The Night Listener',
+				author: 'Armistead Maupin',
+				publisher: 'Harper Colins Publishers',
+				year: 2000,
+				popular: true,
+			},
+			{
+				title: 'Night Tales',
+				author: 'Nora Roberts',
+				publisher: 'Silhouette',
+				year: 2000,
+				popular: false,
+			},
+		]
+		this.popularBooks = this.booksMaster.filter((book) => book.popular === true)
+		this.onChange = this.onChange.bind(this)
+	}
 
-                        <div className="row">
-                            <Cards books = {this.booksMaster} />
+	onChange = (search, searchBy, year) => {
+		this.setState({
+			search: search,
+			searchBy: searchBy,
+			year: year,
+		})
+	}
 
-                        </div>
-                    </div>
-                </main>
-            )
+	componentDidMount() {
+		this.setState({
+			searchBy: 'title',
+		})
+	}
 
-    }
+	render() {
+		return (
+			<main className='container '>
+				<SearchForm onChange={this.onChange} />
+				<div className='books-list'>
+					<div className='row my-3'>
+						<h1 className='h3'>Popular</h1>
+						<hr></hr>
+					</div>
+					<div className='row'>
+						<Cards books={this.popularBooks} />
+					</div>
+				</div>
+				<div className='books-list'>
+					<div className='row my-3'>
+						<h1 className='h3'>Our Books</h1>
+						<hr></hr>
+					</div>
+					<div className='row'>
+						<Cards books={this.booksMaster} />
+					</div>
+				</div>
+			</main>
+		)
+	}
 }
 
-export default Main;
+export default Main
---
+++ src/components/SearchForm.js
@@ -0,0 +1,116 @@
+import React from 'react'
+
+class SearchForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { search: '', searchBy: '', year: 1900 }
+		this.onInputChange = this.onInputChange.bind(this)
+	}
+
+	onInputChange = (event) => {
+		const target = event.target
+		const inputName = target.name
+		const value = target.value
+		this.setState({
+			[inputName]: value,
+		}, () => {
+			this.props.onChange(
+				this.state.search,
+				this.state.searchBy,
+				this.state.year
+			)
+        })
+	}
+
+	componentDidMount() {
+		this.setState({
+			searchBy: 'title',
+		})
+	}
+
+	render() {
+		return (
+			<div className='form-group'>
+				<div className='row mx-auto my-3'>
+					<input
+						type='text'
+						name='search'
+						id='search'
+						placeholder='Search'
+						value={this.state.search}
+						className='form-control col'
+						onChange={this.onInputChange}
+					/>
+					<br />
+				</div>
+				<div className='row mx-auto my-3'>
+					<div className='col row' style={{ lineHeight: '2.5' }}>
+						<div
+							className='form-check col-1 px-0'
+							style={{ display: 'table-cell', verticalAlign: 'middle' }}>
+							<input
+								type='radio'
+								name='searchBy'
+								id='title'
+								value='title'
+								checked={this.state.searchBy === 'title'}
+								onChange={this.onInputChange}
+							/>
+							<label htmlFor='title'>Title</label>
+						</div>
+						<div className='form-check col-1 px-0'>
+							<input
+								type='radio'
+								name='searchBy'
+								id='author'
+								value='author'
+								checked={this.state.searchBy === 'author'}
+								onChange={this.onInputChange}
+							/>
+							<label htmlFor='author'>Author</label>
+						</div>
+						<div className='form-check col-1 px-0'>
+							<input
+								type='radio'
+								name='searchBy'
+								id='publisher'
+								value='publisher'
+								checked={this.state.searchBy === 'publisher'}
+								onChange={this.onInputChange}
+							/>
+							<label htmlFor='publisher'>Publisher</label>
+						</div>
+						<div className='form-check col px-0'>
+							<div className='col' style={{ display: 'inline-block' }}>
+								<input
+									type='radio'
+									name='searchBy'
+									id='titleYear'
+									value='year'
+									checked={this.state.searchBy === 'year'}
+									onChange={this.onInputChange}
+								/>
+								<label htmlFor='titleYear'>Title and Year = </label>
+							</div>
+							<div className='col' style={{ display: 'inline-block' }}>
+								<input
+									type='number'
+									name='yearInput'
+									id='year'
+									minLength={4}
+									min={1900}
+									max={2100}
+									value={this.state.year}
+									className='form-control'
+									onChange={this.onInputChange}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default SearchForm
